86de51dd8c33940ba85e4e79733301e19bf793bc
Den89 <malyhindenis8@gmail.com>
2022-12-24 20:27:21 +0500

    pot

--- js/main.js
+++ js/main.js
@@ -1,16 +1,12 @@
 var burger = document.querySelector('.header__burger');
 var menu = document.querySelector('.menu__list');
+var body = document.querySelector('body');
 
 burger.addEventListener('click', function(){
    burger.classList.toggle('active');
    menu.classList.toggle('active');
+   body.classList.toggle('active');
 });
-
-// var body1 = document.querySelector('.body1'),
-// nextBtn = body1.querySelector('.nextBtn'),
-// backBtn = body1.querySelector('.backBtn'),
-// page10 = body1.querySelector('.page10'),
-// page20 = body1.querySelector('.page20');
 
 
 var body1 = document.querySelector('.body1');
