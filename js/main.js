330bd9bcdb05da72edda39d81a8965cb59d729aa
Den89 <malyhindenis8@gmail.com>
2022-12-24 20:06:29 +0500

    tyr

--- js/main.js
+++ js/main.js
@@ -34,7 +34,23 @@ backBtn.forEach(elem => {
    });
 });
 
-// backBtn.addEventListener.forEach(click, ()=> {
-//    page10.classList.remove('secActive0');
-//    page20.classList.remove('secActive');
-// });
+
+var mse = 'Дом милый дом';
+
+var msg = '<h2>окно браузера</h2><p>ширина - '+ window.innerWidth +'</p>';
+msg +='<p>высота - '+ window.innerHeight +'</p>';
+msg +='<h2>история</h2><p>элементов - '+ window.history.length +'</p>';
+msg +='<h2>экран</h2><p>ширина - '+ window.screen.width +'</p>';
+msg +='<p>высота - '+ window.screen.height +'</p>';
+msg +='<p>высота - '+ mse +'</p>';
+
+var el = document.getElementById('info');
+el.innerHTML = msg;
+
+
+console.log(mse.length);
+console.log(mse.toUpperCase());
+console.log(mse.toLowerCase());
+console.log(mse.charAt(4));
+console.log(mse.indexOf('ом'));
+console.log(mse.lastIndexOf('й'));
