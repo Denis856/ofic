98a6e75a2f8d74e15da7954405440c09fcf8e4fb
Den89 <malyhindenis8@gmail.com>
2023-01-30 12:36:30 +0500

    gg

---
+++ js/main.js
@@ -0,0 +1,45 @@
+var burger = document.querySelector('.header__burger');
+var menu = document.querySelector('.header__menu');
+
+// function menu_show() {
+//    burger.classList.toggle('active');
+//    menu.classList.toggle('active');
+// }
+
+// burger.onclick = menu_show;
+
+burger.addEventListener('click', ()=>{
+   burger.classList.toggle('active');
+   menu.classList.toggle('active');
+});
+
+var filterItem = document.querySelectorAll('.page4__choices');
+var choice = document.querySelectorAll('.choive1');
+var filterImage = document.querySelectorAll('.page4__item');
+
+window.onload = () => { 
+   filterItem.forEach(iten3 => {
+      iten3.onclick = (selectedItem) => {
+         if(selectedItem.target.classList.contains('page4__choice')) {
+            let filterName = selectedItem.target.getAttribute('data-name'); 
+            filterImage.forEach((image) => {
+               let filterImages = image.getAttribute('data-name');
+               if((filterImages == filterName) || filterName == 'all') {
+                  image.classList.remove('hide');
+                  image.classList.add('show');
+               } else {
+                  image.classList.add('hide');
+                  image.classList.remove('show');
+               }
+            });
+         }
+      };
+   });
+};
+
+choice.forEach(item =>{ 
+   item.addEventListener('click', (e) =>{
+      choice.forEach(el=>{ el.classList.remove('active'); });
+   item.classList.add('active');
+});
+});
